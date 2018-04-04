e68fe986c4ec6244b04bebcf711cc3eca7238280
Yuuki <katyt3253@gmail.com>
2018-04-05 00:23:46 +1000

    11

---
+++ homepage.php
@@ -0,0 +1,132 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Homepage</title>
+        <meta name="description" content="homepage">
+        <link rel="stylesheet" href="css/style.css">
+<!--        <link rel="stylesheet" href="css/mobile.css">-->
+        <link rel="stylesheet" href="css/wide.css" media="(min-width: 480px)">
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5ab88174f9a49214"></script>
+        
+    </head>
+    <body>
+        <div id="navigation">
+            <div id="title">
+            <div class="addthis_inline_share_toolbox_20fu"></div>
+            <p>e<span>MEAL</span></p>
+            </div>
+            <nav>
+            <ul>                
+                <li class="active"><a href="">HOME</a></li>
+                <li><a href="">STYLE</a></li>
+                <li><a href="">PURPOSE</a></li>
+                <li><a href="magazines.html">BOX SIZE</a></li>
+                <li><a href="homepage.html">SHOPPING</a></li>
+            </ul>
+            <div class="dropdown"><p>MENU</p>
+                <div class="dropdown-content">
+                    <a href="homepage.html">HOME</a>
+                    <a href="magazines.html">STYLE</a>
+                    <a href="stories.html">PURPOSE</a>
+                    <a href="contactus.html">BOX SIZE</a>
+                    <a href="about.html">SHOPPING</a>
+                </div>
+            </div>
+            <form id="search-form" method="post" >
+                <input type="text" placeholder="Search Here" />
+            </form>
+        </nav>
+        </div>
+        <div id="slidershow">
+        <section class="slider">
+            <ul class="mainslider">
+                <li class="slider-pic slider-pic1"><p>Professional Service</p></li>
+                <li class="slider-pic slider-pic2"><p>Low Fees<br>High Repay</p></li>
+                <li class="slider-pic slider-pic3"><p>Your Money<br>Your Life</p></li>
+            </ul>
+        <div class="focus">
+            <ul class="clearfix">
+                <li><div class="focus-dot focus-dot1"></div></li>
+                <li><div class="focus-dot focus-dot2"></div></li>
+                <li><div class="focus-dot focus-dot3"></div></li>
+            </ul>
+        </div>
+        </section>
+        </div>
+        <div class="white">
+            <p class="subtitle">FIND THE LATEST IDEA</p>
+            <p class="title"><span>LATEST</span> MENU</p>
+            <div class="box-general">
+            <p class="categorybox">Author</p>
+            <img class="lunchbox-img2" src="img/susanna.jpg" alt="">
+            <p class="box-brief"></p>
+            <a class="viewbox" href="story_susanna.html">View More</a>
+        </div>
+        </div>
+        <div class="gray">
+            <p class="subtitle">UNDERSTANDING YOUR SUPER</p>
+            <p class="title"><span>THE</span> MAGAZINE</p>
+            <div id="latest">
+            <p>→ Latest...</p>
+            <div class="img-mag">
+                <img class="pic" src="img/talent.jpg" alt="Does your super have hidden talents?">
+                <p>Does your super have hidden talents?</p>
+                <a href="magazine_superannuation.html">Read More</a>
+            </div>
+                <div class="img-mag">
+                <img class="pic" src="img/cent.jpg" alt="Every cent matters">
+                <p>Every cent matters</p>
+                <a href="magazine_finance.html">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div class="white">
+            <p class="subtitle">WHAT OUR MEMBERS SAY</p>
+            <p class="title">THE <span>STORY</span></p>
+            <div class="story-img">
+                <div class="story"><span>“</span>
+                <p class="story-brief1">The super makes me feel more confident to plan my career. It will never be too early to choose your super.</p>
+                <a href="story_rosalie.html">Find Out More</a>
+                <p class="name">Rosalie Goodwin</p>
+                <p class="position">The college graduate</p>
+                </div>
+                <div class="member-pic">
+                    <img src="img/Rosalie.jpg" alt="Rosalie">
+                </div>
+            </div>
+        </div>
+        <div id="join">
+            <img src="img/joinus.jpg" alt="Join us">
+            <div id="joinbox">
+            <p class="jointitle"><span>JOIN</span> US</p>
+            <p class="detail">Letting the best super to bring you a new future.</p>
+            <a >Sign Up</a>
+            </div>
+        </div>
+        <footer>
+            <div id="contact">
+                <div class="logo-text">
+                    <img src="img/tel.png" alt="Telephone">
+                    <p>(+061) 414 5492 32</p>
+                    <p>(+061) 414 5232 32</p>
+                </div>
+                <div class="logo-text">
+                    <img src="img/mail.png" alt="Mail">
+                    <p>bestsuper1@gmail.com</p>
+                    <p>bestsuper2@gmail.com</p>
+                </div>
+                <div class="logo-text">
+                    <img src="img/twitter.png" alt="Twitter">
+                    <p>@bestsuper1</p>
+                    <p>@bestsuper2</p>
+                </div>
+            </div>
+            <p class="rights">&copy;2017 Best Super Company. All Right Reserved</p>
+        </footer>
+    </body>
+</html>
