5113f1ef2aa1b9df4de402d46858789cbcf71ce9
Yuuki <katyt3253@gmail.com>
2018-04-25 16:11:14 +1000

    April 25

--- LoginTest.php
+++ LoginTest.php
@@ -1,10 +1,22 @@
 <?php
+$con = mysqli_connect('localhost','root','','emeal');
+//if($con){
+//    echo 'Successfully';
+//}
+//    else{
+//        die('Error.');
+//}
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
-if($username == "infs" && $password == "3202"){
+$query = "select * from user WHERE userEmail='$username'";
+$result = $con->query($query) or die($con->error);
+while($row = $result->fetch_assoc()) {
+    if($password == $row['userPassword']){
   echo "Correct";
 }
 else {
   echo "Wrong!";
 }
+}
+
 ?>
